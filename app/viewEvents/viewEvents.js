70ff54e6335e89ac9bc2c7d7a21c4242f5b5c0b7
334849 <334849@01HW293837.India.TCS.com>
2015-07-16 17:20:17 +0530

    Added ajax to get events list.

--- app/viewEvents/viewEvents.js
+++ app/viewEvents/viewEvents.js
@@ -1,14 +1,37 @@
 'use strict';
 
-angular.module('myApp.addEvent', ['ngRoute'])
+angular.module('myApp.viewEvents', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/addEvent', {
-    templateUrl: 'addEvent/addEvent.html',
-    controller: 'AddEventCtrl'
+  $routeProvider.when('/viewEvents', {
+    templateUrl: 'viewEvents/viewEvents.html',
+    controller: 'ViewEventsCtrl'
   });
 }])
 
-.controller('AddEventCtrl', [function() {
+.controller('ViewEventsCtrl','http', [$http,function() {
+$scope.errorMessage = '';
+
+
+
+    var url="http://157.227.99.1:9000/getEvent/"+currentCategory;
+   $http.get(url).success( function(response) {
+$scope.events = response; 
+console.log(" the users list is as belwo : "+response);
+}).failure( function(){
+    console.log(" failed to fetch the data : ");
+    $scope.errorMessage = "failed to fetch the daya "; 
+});
+
+
+
+
+function getDataFromServer(){
+    var url="http://157.227.99.1:9000/getEvent/"+currentCategory;
+   $http.get(url).success( function(response) {
+//                           $scope.students = response; 
+console.log(" the users list is as belwo : "+response);
+                        });
+}
 
 }]);
